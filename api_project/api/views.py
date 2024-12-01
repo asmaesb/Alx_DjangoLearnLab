ac353df6c819f163459d750d63e47401a0efce05
Sabab Asmae <sababasmae@gmail.com>
2024-12-01 17:54:09 +0100

    Task 2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,9 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework.viewsets import ModelViewSet
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
 
+from api_project.api.views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 from django.urls import include, path
 
 urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-     path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
