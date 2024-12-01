505cc10ce48945d7f325f6f05d0fdaa22761dc11
Sabab Asmae <sababasmae@gmail.com>
2024-12-01 18:03:57 +0100

    Task 2 -fix 2

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,31 +1,14 @@
-"""
-URL configuration for api_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-
-from api_project.api.views import BookList, BookViewSet
-
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
 from django.urls import include, path
+from api.views import BookViewSet
 
+# Initialize DefaultRouter
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+# Define urlpatterns
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include(router.urls)),  # Includes all router-defined routes under 'api/'
+    path('admin/', admin.site.urls),    # Admin site
 ]
