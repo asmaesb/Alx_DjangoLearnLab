c34ddee2009bae2a006116608023b1d86c919876
Sabab Asmae <sababasmae@gmail.com>
2024-12-01 18:21:03 +0100

    task 3-fix 3

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     "rest_framework.authtoken",
     "rest_framework.authentication.TokenAuthentication",
+    "rest_framework.authentication.TokenAuthentication",
+
     'api',
 ]
 
